076a39d2d2a91b495ce3971206515a31d241a42f
compromise-satisfaction <44921951+compromise-satisfaction@users.noreply.github.com>
2019-10-31 10:47:31 +0900

    a

--- js/main.js
+++ js/main.js
@@ -234,49 +234,6 @@ function Load(width,height){
       var MakeScene = function(){
       var scene = new Scene();                                // 新しいシーンを作る
 
-<<<<<<< HEAD
-      var Hand = new Sprite(1,1);
-      Hand.image = core.assets["image/Main.png"];
-      scene.addChild(Hand);
-
-      var Main = new Sprite(465,465);
-      Main.image = core.assets["image/Main.png"];
-      Main.x = 0;
-      Main.y = 0;
-      scene.addChild(Main);
-
-      var Start = new Sprite(45,45);
-      Start.image = core.assets["image/Number.png"];
-      Start.x = width-45;
-      Start.y = height-45;
-      Start.frame = 10;
-      scene.addChild(Start);
-
-      var Numbers = Class.create(Sprite, {
-        initialize: function(x,y,z,o) {
-          Sprite.call(this,45,45);
-          this.image = core.assets["image/Number.png"];
-          this.x = x;
-          this.y = y;
-          this.frame = z;
-          if(o) this.opacity = 0.5;
-          scene.addChild(this);
-        }
-      });
-
-      var I = 0;
-      var K = 0;
-      var Choice = 0;
-
-      for (var i = 1; i < 82; i++){
-        Number[i] = new Numbers(5+K,5+I,0);
-        K = K+50;
-        if(i%3==0) K = K+5;
-        if(i%9==0){
-          I = I+50;
-          if(i%27==0) I = I+5;
-          K=0;
-=======
       var Hava = (width-505)/2;
       var Cont = 0;
       var Sides = [];
@@ -329,18 +286,8 @@ function Load(width,height){
         for (var i = 0; i < 12; i++){
           Objects[i] = new Objects(i,10,i);
           scene.addChild(Objects[i]);
->>>>>>> 9ded5849fc5a558585064a7810d3aa6bd176bd5d
         }
 
-<<<<<<< HEAD
-      for (var i = 1; i < 10; i++){
-        Set[i] = new Numbers((i-1)*50,500,i,true);
-      }
-
-      if(V!=undefined){
-        for (var i = 1; i < 82; i++){
-          Number[i].frame = V[i-1];
-=======
         var Start_button = new Sprite(190,38);
         Start_button.image = core.assets["image/Start_button.png"];
         Start_button.x = width-190-width/2+190/2;//Hava + 505-252;
@@ -564,7 +511,6 @@ function Load(width,height){
             E_I2++;
             E_D(Enemys[E_I-1]);
           }
->>>>>>> 9ded5849fc5a558585064a7810d3aa6bd176bd5d
         }
 
         function E_U(name){
@@ -676,14 +622,6 @@ function Load(width,height){
             }
           }
         }
-<<<<<<< HEAD
-        if(Start.intersect(Hand)){
-          var V = [];
-          for (var i = 0; i < 81; i++){
-            V[i] = Number[i+1].frame;
-          }
-          core.replaceScene(AnswerScene(V));
-=======
 
         function randam_haiti(){
           for (var i = 0; i < S; i++) {
@@ -691,57 +629,8 @@ function Load(width,height){
             S1[i] = rand(7);
           }
           kabeset();
->>>>>>> 9ded5849fc5a558585064a7810d3aa6bd176bd5d
         }
 
-<<<<<<< HEAD
-      var Hand = new Sprite(1,1);
-      Hand.image = core.assets["image/Main.png"];
-      scene.addChild(Hand);
-
-      var Start = new Sprite(45,45);
-      Start.image = core.assets["image/Number.png"];
-      Start.x = width-45;
-      Start.y = height-45;
-      Start.frame = 11;
-      scene.addChild(Start);
-
-      var Main = new Sprite(465,465);
-      Main.image = core.assets["image/Main.png"];
-      Main.x = 0;
-      Main.y = 0;
-      scene.addChild(Main);
-      var Numbers = Class.create(Sprite, {
-        initialize: function(x,y,z) {
-          Sprite.call(this,45,45);
-          this.image = core.assets["image/Number.png"];
-          this.x = x;
-          this.y = y;
-          this.frame = z;
-          scene.addChild(this);
-        }
-      });
-
-      var label = new Label();
-        label.x = 0;
-        label.y = 470;
-        label.color = 'red';
-        label.font = '40px "Arial"';
-        label.text = ("");
-        scene.addChild(label);
-
-      var I = 0;
-      var K = 0;
-
-      for (var i = 1; i < 82; i++){
-        Number[i] = new Numbers(5+K,5+I,V[i-1]);
-        K = K+50;
-        if(i%3==0) K = K+5;
-        if(i%9==0){
-          I = I+50;
-          if(i%27==0) I = I+5;
-          K=0;
-=======
         function set(x,y,z){
           if(z==8||z==9||z==10||z==11){
             for (var i = 0; i < 10; i++) {
@@ -850,20 +739,8 @@ function Load(width,height){
             }
           }
           kabeset();
->>>>>>> 9ded5849fc5a558585064a7810d3aa6bd176bd5d
         }
 
-<<<<<<< HEAD
-      Main.addEventListener("enterframe",function(){
-        var e = 0;
-        var f = 0;
-        for (var d = 0; d < 9; d++){
-          for (var i = 1+d*9; i < 10+d*9; i++){
-            for(var c = 1; c < 10; c++){
-              if(V[i-1]==c){
-                for (var k = 1+d*9; k < 10+d*9; k++){
-                  if((V[k-1]+"").length==9&&V[k-1]!="000000009") V[k-1] = V[k-1].substring(0,c-1) + "0" + V[k-1].substring(c);
-=======
         function idou_E(){
           if (core.input.up){
             Enemy_moves[E_I-1][E_I2] = "↑";
@@ -1422,7 +1299,6 @@ function Load(width,height){
                     scene.removeChild(Sides[i]);
                     S1[i] = 20;
                   }
->>>>>>> 9ded5849fc5a558585064a7810d3aa6bd176bd5d
                 }
                 for (var i = 0; i < V; i++) {
                   if(V1[i]==2){
@@ -1463,27 +1339,6 @@ function Load(width,height){
                 Verticals[i].image = core.assets["image/side.png"];
                 scene.addChild(Verticals[i]);
               }
-<<<<<<< HEAD
-            }
-          }
-          for (var i = 1+d; i < 74+d; i = i+9){
-            for(var c = 1; c < 10; c++){
-              if(V[i-1]==c){
-                for (var k = 1+d; k < 74+d; k = k+9){
-                  if((V[k-1]+"").length==9&&V[k-1]!="000000009") V[k-1] = V[k-1].substring(0,c-1) + "0" + V[k-1].substring(c);
-                }
-              }
-            }
-          }
-          if(d==3||d==6){
-            e = e+27;
-            f = 0;
-          }
-          for (var i = 1+e+f*3; i < 22+e+f*3; i++){
-            if(i==4+e+f*3||i==13+e+f*3){
-              i = i+5;
-              continue;
-=======
               else if(V1[i]==2||V1[i]==20){
                 V1[i]=2;
                 Verticals[i].image = core.assets["image/door.png"];
@@ -1589,7 +1444,6 @@ function Load(width,height){
               else {
                 if(name.frame==0||name.frame==4) name.x-=50;
               }
->>>>>>> 9ded5849fc5a558585064a7810d3aa6bd176bd5d
             }
             if (Enemy_moves[k][Time_es[k]]=="→"){
               for (var i = 0; i < V; i++) {
@@ -1599,41 +1453,6 @@ function Load(width,height){
                     Dame = "→";
                     return;
                   }
-<<<<<<< HEAD
-                  if((V[k-1]+"").length==9&&V[k-1]!="000000009") V[k-1] = V[k-1].substring(0,c-1) + "0" + V[k-1].substring(c);
-                }
-              }
-            }
-          }
-          f++;
-        }
-        var e = 0;
-        var f = 0;
-        for (var d = 0; d < 9; d++){
-          if(d==3||d==6){
-            e = e+27;
-            f = 0;
-          }
-          for (var i = 1+e+f*3; i < 22+e+f*3; i++){
-            if(i==4+e+f*3||i==13+e+f*3){
-              i = i+5;
-              continue;
-            }
-            var N = [0,0,0,0,0,0,0,0,0];
-            var S = [0,0,0,0,0,0,0,0,0];
-            for (var k = (1+e)+f*3; k < (22+e)+f*3; k++){
-              if(k==4+e+f*3||k==13+e+f*3){
-                k = k+5;
-                continue;
-              }
-              if(V[k-1].length==9){
-                for (var c = 0; c < 9; c++){
-                  if(V[k-1].substring(c,c+1)==c+1){
-                    N[c]++;
-                    S[c] = k-1;
-                  }
-=======
->>>>>>> 9ded5849fc5a558585064a7810d3aa6bd176bd5d
                 }
               }
               if(name.y<5||name.y>455);
@@ -1643,66 +1462,6 @@ function Load(width,height){
               }
             }
           }
-<<<<<<< HEAD
-          for (var c = 0; c < 9; c++){
-            if(N[c] == 1) V[S[c]] = c+1;
-          }
-          f++;
-        }
-        /*
-        for (var d = 0; d < 9; d++){
-          for (var i = 1+d*9; i < 10+d*9; i++){
-            var N = [0,0,0,0,0,0,0,0,0];
-            var S = [0,0,0,0,0,0,0,0,0];
-            for (var k = 1+d*9; k < 10+d*9; k++){
-              if(V[k-1].length==9){
-                for (var c = 0; c < 9; c++){
-                  if(V[k-1].substring(c,c+1)==c+1){
-                    N[c]++;
-                    S[c] = k-1;
-                  }
-                }
-              }
-            }
-          }
-          for (var c = 0; c < 9; c++){
-            if(N[c] == 1) V[S[c]] = c+1;
-          }
-        }
-        for (var d = 0; d < 9; d++){
-          for (var i = 1+d; i < 74+d; i = i+9){
-            var N = [0,0,0,0,0,0,0,0,0];
-            var S = [0,0,0,0,0,0,0,0,0];
-            for (var k = 1+d; k < 74+d; k = k+9){
-              if(V[k-1].length==9){
-                for (var c = 0; c < 9; c++){
-                  if(V[k-1].substring(c,c+1)==c+1){
-                    N[c]++;
-                    S[c] = k-1;
-                  }
-                }
-              }
-            }
-          }
-          for (var c = 0; c < 9; c++){
-            if(N[c] == 1) V[S[c]] = c+1;
-          }
-        }
-        */
-        for (var i = 1; i < 82; i++){
-          if(V[i-1]=="100000000") V[i-1] = 1;
-          if(V[i-1]=="020000000") V[i-1] = 2;
-          if(V[i-1]=="003000000") V[i-1] = 3;
-          if(V[i-1]=="000400000") V[i-1] = 4;
-          if(V[i-1]=="000050000") V[i-1] = 5;
-          if(V[i-1]=="000006000") V[i-1] = 6;
-          if(V[i-1]=="000000700") V[i-1] = 7;
-          if(V[i-1]=="000000080") V[i-1] = 8;
-          if(V[i-1]=="000000009") V[i-1] = 9;
-          Number[i].a = V[i-1];
-          for (var k = 1; k < 10; k++){
-            if(V[i-1]==k) Number[i].frame = k;
-=======
 
           Explosion.addEventListener("enterframe",function(){
             Explosion.frame++;
@@ -1876,19 +1635,10 @@ function Load(width,height){
           if(Stage_Number == stage){
             Stage_Number++;
             window.localStorage.setItem("stage",Stage_Number);
->>>>>>> 9ded5849fc5a558585064a7810d3aa6bd176bd5d
           }
           scene.addChild(Next_button);
         }
 
-<<<<<<< HEAD
-      scene.on("touchstart",function(e){
-        Hand.x = e.x;
-        Hand.y = e.y;
-        for (var i = 1; i < 82; i++){
-          if(Number[i].intersect(Hand)){
-            label.text = Number[i].a;
-=======
         var S_Input = new Entity();
         S_Input.moveTo(10,40);
         S_Input.width = 190;
@@ -1910,7 +1660,6 @@ function Load(width,height){
           Hand.y = e.y;
           if(Start_button.intersect(Hand)){
             core.replaceScene(GameScene(text));
->>>>>>> 9ded5849fc5a558585064a7810d3aa6bd176bd5d
           }
           if(Menu_button.intersect(Hand)){
             core.replaceScene(MenuScene());
